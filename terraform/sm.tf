0099d905b6d5925196e544436949ece16cc3af00
bear-jordan <bearjordan@gmail.com>
2025-10-08 20:45:57 +0000

    enh: add secrets store

---
+++ terraform/sm.tf
@@ -0,0 +1,35 @@
+resource "aws_secretsmanager_secret" "renovate_secrets" {
+  name                    = "homelab/renovate"
+  description             = "renovate secrets"
+  recovery_window_in_days = 7
+  tags = merge(
+    var.default_tags,
+    {
+      Name = "secret-manager"
+    }
+  )
+}
+
+resource "aws_secretsmanager_secret" "tailscale_secrets" {
+  name                    = "homelab/tailscale"
+  description             = "tailscale secrets"
+  recovery_window_in_days = 7
+  tags = merge(
+    var.default_tags,
+    {
+      Name = "secret-manager"
+    }
+  )
+}
+
+resource "aws_secretsmanager_secret" "grafana_secrets" {
+  name                    = "homelab/grafana"
+  description             = "grafana secrets"
+  recovery_window_in_days = 7
+  tags = merge(
+    var.default_tags,
+    {
+      Name = "secret-manager"
+    }
+  )
+}
