d9c45e40098f4e203eaf6477725cf2edb31a33c9
bear-jordan <bearjordan@gmail.com>
2025-12-06 22:39:21 +0000

    maint; depricate unused secret

--- terraform/sm.tf
+++ terraform/sm.tf
@@ -1,15 +1,3 @@
-resource "aws_secretsmanager_secret" "aws_sm_secrets" {
-  name                    = "external-secrets/aws-sm-secrets"
-  description             = "secrets manager access"
-  recovery_window_in_days = 7
-  tags = merge(
-    var.default_tags,
-    {
-      Name = "secret-manager"
-    }
-  )
-}
-
 resource "aws_secretsmanager_secret" "renovate_secrets" {
   name                    = "homelab/renovate"
   description             = "renovate secrets"
